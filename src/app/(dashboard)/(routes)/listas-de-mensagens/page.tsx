d2099d97732427173568378387d5de7ee214c263
Augusto <gutodiasdev@gmail.com>
2024-10-08 09:37:49 -0300

    refact: adjust logout button and messages lists page

--- src/app/(dashboard)/(routes)/listas-de-mensagens/page.tsx
+++ src/app/(dashboard)/(routes)/listas-de-mensagens/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
         <PageHeader>
           Listas de Mensagens
         </PageHeader>
-        <div className="my-4 grid md:grid-cols-5 gap-4">
+        <div className="my-4 grid md:grid-cols-3 gap-4">
           <div className="border-2 p-2 rounded-md flex flex-col gap-2 md:h-80 border-gray-400 border-dashed hover:cursor-pointer hover:bg-slate-100 transition-all ease-in-out">
             <div className="flex items-center justify-center h-full w-full">
               <div className="flex flex-col items-center">
@@ -43,7 +43,7 @@ export default function Page() {
               </div>
             </div>
           </div>
-          {Array.from({ length: 4 }).map((item, index) => (<Skeleton key={index} className="p-4 rounded-md md:h-80 bg-slate-200" />))}
+          {Array.from({ length: 2 }).map((item, index) => (<Skeleton key={index} className="p-4 rounded-md md:h-80 bg-slate-200" />))}
         </div>
       </section>
     )
@@ -55,7 +55,7 @@ export default function Page() {
         <PageHeader>
           Listas de Mensagens
         </PageHeader>
-        <div className="grid gap-4 xl:grid-cols-4 2xl:grid-cols-5">
+        <div className="grid gap-4 xl:grid-cols-3 2xl:grid-cols-3">
           <div className="border-2 p-2 rounded-md flex flex-col gap-2 md:h-80 border-gray-400 border-dashed hover:cursor-pointer hover:bg-slate-100 transition-all ease-in-out" onClick={() => setOpen(true)}>
             <div className="flex items-center justify-center h-full w-full">
               <div className="flex flex-col items-center">
--- src/components/sidebar/root.tsx
+++ src/components/sidebar/root.tsx
@@ -95,10 +95,12 @@ export function Root() {
           ))}
         </div>
       </div>
-      <Button variant="ghost" onClick={handleLogout}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out mr-2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
-        Sair
-      </Button>
+      <div className="flex w-full">
+        <Button variant="ghost" onClick={handleLogout}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out mr-2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
+          Sair
+        </Button>
+      </div>
     </div>
   )
 }
